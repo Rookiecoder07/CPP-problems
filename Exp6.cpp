910bc86fa3731ef24b4f27023c000c9ac2fac537
AK <aksingh252007@gmail.com>
2026-04-27 20:19:32 +0530

    Dry run

--- Exp6.cpp
+++ Exp6.cpp
@@ -4,7 +4,10 @@ using namespace std;
 void fibonacciIterative(int n){
     int a=0,b=1;
     cout << "Fibonacci series (Iterative) up to " << n << ": ";
-    while(a<=n){ cout<<a<<" "; int next=a+b; a=b; b=next; }
+    while(a<=n){ cout<<a<<" "; 
+        int next=a+b;
+            a=b; 
+            b=next; }
     cout<<"\n";
 }
 void fibonacciRecursivePrint(int a,int b,int n){
